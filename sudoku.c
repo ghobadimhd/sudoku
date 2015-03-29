981b5c7041505a4b46c5cea8b9cad804a3ab3ecb
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-03-30 01:58:24 +0430

    intialing sudoku puzzle solver

---
+++ sudoku.c
@@ -0,0 +1,16 @@
+/*
+sudoku	2015-03-30 01:03
+Program name : sudoku game
+Author : Mohammad Ghobadi
+Email : ghobadi.mhd@outlook.com
+describtion : This is program is a sudoku solver .
+*/
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	// getting a usolved puzzle 
+	// solving puzzle 
+	// printing puzzle 
+	return 0;
+}
