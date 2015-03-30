390a458e7c2eb1b2afa65528982d6ffdee85ba58
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-03-30 10:33:45 +0430

    cell's column tester function added

--- tester.h
+++ tester.h
@@ -13,3 +13,17 @@ int rowTest(int row , int column)
 		return 0 ;
 	return 1 ;
 }
+/*
+this is function for testing a cell in it's column .
+*/
+int columnTest(int row , int column)
+{
+	int cellValue = puzzle[row][column] , count = 0;
+	if(cellValue != 0) 
+		// count same cell value in it's column 
+		for(int i = 0 ; i < 9 ; i++)
+			if (puzzle[i][column] == cellValue)
+					count++;
+	if (count > 1 )	// there is repeated value in column 
+		return 0 ;
+}
