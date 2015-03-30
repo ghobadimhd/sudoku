4715db12c7bc2f65b477354c4e3bc309b966cfcb
Ghobadimhd <Ghobadi.mhd@outlook.com>
2015-03-30 13:10:40 +0430

    backtrack function added

---
+++ solver.h
@@ -0,0 +1,30 @@
+/*
+this is function for solving puzzle in backtrack way .
+*/
+int backtrack(int cellNumber)
+{
+	if ( cellNumber < 81 ) 
+	{
+		printf("%d  ", cellNumber);
+		//debug 
+		int row = cellNumber / 9  ,
+		column = row + (cellNumber % 9 ) ;
+		if(puzzle[row][column] == 0)
+		{
+			for(int i=1 ; i < 10 ; i++) 
+			{
+				
+					puzzle[row][column]=i;
+					if(cellTest(row , column));
+						if(! backtrack(cellNumber + 1) )
+							return 1;
+			}
+			puzzle[row][column]=0;
+			return 0 ; // no match 
+		}
+		else
+			if(! backtrack(cellNumber + 1) )
+				return 1;
+	}
+	return 1 ; 
+}
